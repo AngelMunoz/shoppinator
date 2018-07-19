d05bc38c00c06f18e4130e0448cb0def653abfbf
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2018-07-19 17:45:41 -0500

    renombrar las rutas

--- src/routes.ts
+++ src/routes.ts
@@ -4,21 +4,21 @@ import { RouteConfig } from "aurelia-router";
 export const ROUTES: RouteConfig[] = [
   {
     route: ["", "home"],
-    title: "Home",
+    title: "Inicio",
     name: "home",
     nav: true,
     moduleId: PLATFORM.moduleName("./pages/home"),
   },
   {
     route: "my-lists",
-    title: "My Lists",
+    title: "Mis Listas",
     name: "lists",
     nav: true,
     moduleId: PLATFORM.moduleName("./pages/my-lists"),
   },
   {
     route: "my-lists/:id",
-    title: "My List Details",
+    title: "Mi Lista Detallada",
     name: "listdetail",
     nav: false,
     moduleId: PLATFORM.moduleName("./pages/list-detail"),
